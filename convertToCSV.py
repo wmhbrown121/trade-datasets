da6204739e7d2b62ffffe588f90bbc842a3a9fb9
Hogan <wmhbrown121@live.com>
2024-06-08 18:06:28 -0500

    convert using Python os and csv libs

--- convertToCSV.py
+++ convertToCSV.py
@@ -8,14 +8,19 @@ import os
 directory = './Data/Stocks'
 files = os.listdir(directory)
 index = 0
-while index < 10:
+while index < 2:
     filename = files[index]
     if filename.endswith('.txt'):
-        # print(filename+' - '+filename[:-7])
-        file = pd.read_fwf(directory+'/'+filename)
+        print(filename+' - '+filename[:-7])
+        # file = pd.read_fwf(directory+'/'+filename)
         # with open(os.path.join(directory, filename)) as f:
         #     print(f.read())
-        file.to_csv('./Stocks/'+filename[:-7]+'.csv',index=None) 
+
+        with open(os.path.join(directory, filename)) as in_file:
+            stripped = (line.strip() for line in in_file)
+            lines = (line.split(",") for line in stripped if line)
+            with open('./Stocks/'+filename[:-7]+'.csv', 'w') as out_file:
+                writer = csv.writer(out_file)
+                writer.writerows(lines)
+        # file.to_csv('./Stocks/'+filename[:-7]+'.csv',index=None) 
     index += 1
-
-
